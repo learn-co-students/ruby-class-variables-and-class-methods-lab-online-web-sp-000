ffa25e5e050d49c8c9226ec099b0b12e40822685
Cheng Vue <cvu3@ucsc.edu>
2018-11-26 08:58:32 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genres_hash = {}
+
+    @@genres.each do |genre|
+      genres_hash[genre] == nil ? genres_hash[genre] = 1 : genres_hash[genre] += 1
+    end
+
+    genres_hash
+  end
+
+  def self.artist_count
+    artists_hash = {}
+
+    @@artists.each do |artist|
+      artists_hash[artist] == nil ? artists_hash[artist] = 1 : artists_hash[artist] += 1
+    end
+
+    artists_hash
+  end
+end
