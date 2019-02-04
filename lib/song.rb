d24b78a2e95d0fb780007ddf45cb49febd2e2c14
Sandesh Karki <karkisandesh@hotmail.com>
2019-02-04 21:35:33 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ end
   
   def self.artist_count 
      artist_count = { }
-     @@artists.map do |artist|
+     @@artists.each do |artist|
        if artist_count[artist]
           artist_count[artist] += 1
         else
