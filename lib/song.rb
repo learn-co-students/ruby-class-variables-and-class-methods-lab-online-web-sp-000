55643ba841a0eb28e7219a7e1b666bb178a5c9f3
Kimberly Rouse <ksrouse94@gmail.com>
2019-03-30 00:28:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,13 +30,34 @@ class Song
   end  
   
   def self.genres
-    @@genres.each do |genre|
-      if !@@genres.include?(@genre) || @genre != nil
-        @@genres << @genre
-      end
-    end  
-    @@genres
+    @@genres.uniq
   end  
   
+  def self.artists
+    @@artists.uniq
+  end  
   
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      if !hash[genre].nil?
+        hash[genre] += 1 
+      else
+        hash[genre] = 1
+      end   
+    end  
+    hash
+  end
+  
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if !hash[artist].nil?
+        hash[artist] += 1 
+      else
+        hash[artist] = 1 
+      end
+    end 
+    hash
+  end  
 end  
