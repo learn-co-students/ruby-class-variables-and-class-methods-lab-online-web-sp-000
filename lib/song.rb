969e6c1c82de05c5976f8496135f5cfd7828a8be
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-02 17:27:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -40,4 +40,15 @@ class Song
       genre_count
   end
   
+  def self.artist_count
+    artist_count = {}
+    @@artists.collect do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] = 1 
+      end 
+    end 
+      artist_count
+  end   
 end 
