20659812a3194dc1d64af3a73de428da4fbe0e91
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-26 21:09:27 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,14 +24,15 @@ class Song
     @@genre.uniq
   end
 
-def self.genre_count
-  genre_count = Hash.new(0)
-  @@genre.each {|genre| genre_count[genre] += 1}
-  genre_count
-end
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genre.each {|genre| genre_count[genre] += 1}
+    genre_count
+  end
 
-def self.artist_count
-  artist_count = Hash.new(0)
-  @@artist.each {|artist| artist_count[artist] +=1}
-  artist_count
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artist.each {|artist| artist_count[artist] +=1}
+    artist_count
+  end
 end
