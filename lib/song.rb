82e1e8d006aca03385e1d0371aa298ff94ca31af
Alan Krajina <alankrajina88@gmail.com>
2019-05-19 11:42:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,20 @@ class Song
     @@artists.group_by { |v| v }.transform_values(&:size)
   end
   
+=begin
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end  
+=end  
+  
 end
 
 
