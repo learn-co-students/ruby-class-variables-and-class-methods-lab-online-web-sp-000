f66c24328d1b32e445968496b8cf643938899161
Corey H <c.l.halicke@gmail.com>
2019-06-14 02:45:51 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+end
+
+# song class must produce name, artist and genre
