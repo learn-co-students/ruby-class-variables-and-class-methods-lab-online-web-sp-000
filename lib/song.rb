8040eb33c368c4936883e29b50cf8e38d95e3454
Connor Cowan <ccowan003@comcast.net>
2019-06-26 00:04:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,53 @@
 class Song
-
   attr_accessor :name, :artist, :genre
-  @@count = 0                           #Class Variable counter initialization
 
-  def intialize(name, artist, genre)
+  @@count = 0                           #Class Variable counter initialization
+  @@artists = []                        #initialization of our arrays
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
-    genres
+    @@genres<< genre
+    @@artists << artist                 #add created artists to our artists array
+
   end
 
-  def self.count        #class method
+  def self.count                #ALL class methods have self. in it
     return @@count
   end
 
   def self.genres
-    @@genres = []
-
+    return @@genres.uniq        #uniq method returns unique genres only
   end
 
   def self.artists
-    @@artists[artist]
-  end
-  def artist_count
-
+    return @@artists.uniq       #returns an array of only unique artists
   end
 
-  def genre_count
-
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    return artist_count
   end
+
+  def self.genre_count
+    genre_count = {}              #init genre_count hash
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1    #add to existing genre in hash
+      else
+        genre_count[genre] = 1    #add new genre
+      end
+    end
+    return genre_count            #returns our hash
+  end
+end
