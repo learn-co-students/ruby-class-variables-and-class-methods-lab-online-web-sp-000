13d20aeefb8e63e4d14b1f433dc35dd209ad5b69
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-01 00:01:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,26 @@ class Song
  @@artists = []
  @@genres = []
 
+ attr_reader :name, :artist, :genre
+
   def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
     @@count +=1
     @@artists << artist
     @@genres << genre
   end
+
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists
+  end
+  def self.genres
+    @@genres
+  end
+
+
 end
