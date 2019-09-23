1e2ff432f772ed4580d0e3601866d6bc5848a9ef
Adam Weissman <adam@comedytragedyepic.com>
2019-09-23 02:39:38 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genres
+    @@genres.uniq!
+  end
+  
+  def self.genre_count
+    Hash.new(0).tap {|h| @@genres.each { |genre| h[genre] += 1 } }
+  end
+  
+  def self.artist_count
+    Hash.new(0).tap {|h| @@artists.each { |artist| h[artist] += 1 } }
+  end
+  
+end
+
+  
