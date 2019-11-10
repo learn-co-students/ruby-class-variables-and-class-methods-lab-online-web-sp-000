564489a2443eb9913979f414811d0ff48f29d0e9
Ryan M <rmccauleymusic@gmail.com>
2019-11-10 22:03:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  attr_accessor :name, :artist, :genre
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) {|total, i| total[i] += 1; total}
+  end
+
+
+end
