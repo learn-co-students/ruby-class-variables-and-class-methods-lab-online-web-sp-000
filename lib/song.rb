baaeb7b8072854e9311d9ab77a8705fbc638ba62
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-14 18:55:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,10 +30,39 @@ class Song
   end 
   
   def self.genre_count
-    Hash[*@@genres.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
-    end
-    
-    def self.artist_count
-      Hash[*@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+    genre_count = {}
+    @@genres.each do |artist|
+      if genre_count[artist]
+        genre_count[artist] += 1 
+      else 
+        genre_count[artist] = 1
+      end 
     end 
+    genre_count
+  end
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] = 1 
+      end 
+    end 
+    artist_count
+  end 
 end 
