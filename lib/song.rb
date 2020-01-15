acf1ffd11471148e38431b324c330e3a9acc564a
Aoune Syed <aoune007@gmail.com>
2020-01-15 06:04:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,20 +6,57 @@ class Song
     @@artists = []
 
 
-  def initialize(name, artist, genre)
-      @name = name
-      @artist = artist
-      @genre = genre
-      @@count +=1
-      @@genres << genre
-      @@artists << artist
-  end
+      def initialize(name, artist, genre)
+          @name = name
+          @artist = artist
+          @genre = genre
+          @@count +=1
+          @@genres << genre
+          @@artists << artist
+      end
 
+        def self.count
+          @@count
+        end
 
+          def self.artists
+            @@artists.uniq
+          end
 
+            def self.genres
+              @@genres.uniq
+            end
 
+              def self.artist_count
+                  artist_count = {}
+                  @@artists.each do |artist|
+                    if artist_count[artist]
+                      artist_count[artist] += 1
+                    else
+                      artist_count[artist] = 1
+                    end
+                  end
+                  artist_count
+                end
 
-
-
-
+                def self.genre_count
+                    genre_count = {}
+                    @@genres.each do |genre|
+                      if genre_count[genre]
+                        genre_count[genre] += 1
+                      else
+                        genre_count[genre] = 1
+                      end
+                    end
+                    genre_count
+                  end
 end
+#def add_student(student_name, grade)
+#
+#if roster[grade]
+#   roster[grade] << student_name
+#else
+#   roster[grade] = []  #set roster[grade] equal to an empty array if it doesnt already exist
+#   roster[grade] << student_name
+#end
+#end
