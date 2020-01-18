e4bb99a74edbb62d8a50bcbd88ac5078d6859f96
LIManuel <leonmalisov@gmail.com>
2020-01-18 15:59:34 -0500

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -5,6 +5,9 @@ describe "Song" do
   let!(:lucifer) { Song.new("Lucifer", "Jay-Z", "rap" ) }
   let!(:ninety_nine_problems) { Song.new("99 Problems", "Jay-Z", "rap") }
   let!(:hit_me) { Song.new("hit me baby one more time", "Brittany Spears", "pop") }
+
+  # let!(:borp) { Song.new("hit me baby two more time", "Brittany Spears", "pop") }
+  # let!(:dorp) { Song.new("hit me baby three more time", "Brittany Spears", "pop") }
  
   after(:each) do 
     Song.class_variable_set(:@@artists, [])
