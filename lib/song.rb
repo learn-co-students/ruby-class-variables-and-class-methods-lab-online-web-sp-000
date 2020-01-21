672610a390112ba64ea66a323a349f9906cb72da
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-21 07:13:47 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,15 @@ class Song
 @@count = 0
 @@artists = []
 
+
 def initialize(name, artist, genre)
-  @@count += 1
-  @@genres << genre
-  @@artists << artist
   @name = name
   @artist = artist
   @genre = genre
+  @@count += 1
+  @@genres << genre
+  @@artists << artist
+  
   end
 
 def self.count
@@ -27,6 +29,30 @@ def self.artists
   end
   
 def self.genre_count
-  
-  end
+   genre_count = {}
+	 @@genres.each do |genre|
+	    if genre_count[genre] 
+			   genre_count[genre] += 1
+			else
+			   genre_count[genre] = 1
+			end
+			
+	end
+	genre_count
+	end
+	
+def self.artist_count
+  artist_count = {}
+	 @@artists.each do |artist|
+	    if artist_count[artist] 
+			   artist_count[artist] += 1
+			else
+			   artist_count[artist] = 1
+			end
+	end
+	artist_count
+	end
+	
+	
+	
 end
