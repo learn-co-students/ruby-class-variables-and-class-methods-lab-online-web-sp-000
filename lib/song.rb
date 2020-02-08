9f9682a6c56dfe08b0ae1737233b29be4a146de2
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-08 20:22:55 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if !genre_hash.key?(genre)
+        genre_hash[genre] = 1
+      elsif genre_hash.key?(genre)
+        genre_hash[genre] += 1
+      end
+    end
+    genre_hash
+  end
+  
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if !artist_hash.key?(artist)
+        artist_hash[artist] = 1
+      elsif artist_hash.key?(artist)
+        artist_hash[artist] += 1
+      end
+    end
+    artist_hash
+  end
+  
+end
