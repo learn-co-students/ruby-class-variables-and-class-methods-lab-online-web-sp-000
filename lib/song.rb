20edba7082456acfd0b110ac236a1c6c5945cae7
Will Corcoran <corcoranwill@gmail.com>
2020-03-12 16:02:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,26 +28,26 @@ class Song
   end
 
   def self.artist_count
-    artist_hash = {}
+    artist_count = {}
     @@artists.each do |artist|
-      if artist_hash[artist] == nil
-        artist_hash[artist] = 1
+      if artist_count[artist]
+        artist_count[artist] += 1
       else
-        artist_hash[artist] += 1
+        artist_count[artist] = 1
       end
     end
-    artist_hash
+    artist_count
   end
 
   def self.genre_count
-    genre_hash = {}
+    genre_count = {}
     @@genres.each do |genre|
-      if genre_hash[genre] == nil
-        genre_hash[genre] = 1
+      if genre_count[genre]
+        genre_count[genre] += 1
       else
-        genre_hash[genre] += 1
+        genre_count[genre] = 1
       end
     end
-    genre_hash
+    genre_count
   end
 end
