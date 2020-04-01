d45170776b8f028565a7ed418bdc3a341349130a
Micah McCallum <micahmccallum@outlook.com>
2020-04-01 14:29:40 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@artists = []
+  @@genres = []
+  @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    genres = []
+    @@genres.each do |genre|
+      genres << genre unless genres.include?(genre)
+    end
+    genres
+  end
+
+  def self.artists
+    artists = []
+    @@artists.each do |artist|
+      artists << artist unless artists.include?(artist)
+    end
+    artists
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] = @@genres.count(genre) unless genre_count.include?(genre)
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      artist_count[artist] = @@artists.count(artist) unless artist_count.include?(artist)
+    end
+    artist_count
+  end
+
+end
