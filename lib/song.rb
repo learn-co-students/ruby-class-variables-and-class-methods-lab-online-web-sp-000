33f97a1664513bfbb4e71b8b6499bf485fbbe7fc
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 23:07:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,33 +2,52 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres =[]
 
   def initialize(name, artist, genre)
     @name = name
-    @@count += 1
-    @genre = genre
-    @@genres << genre
     @artist = artist
-    @@artists << artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
   end
 
   def self.count
     @@count
   end
 
-  def self.genres
-    @@genres.uniq
-  end
-  def self.genre_count
-    Hash[@@genres.uniq.collect {|g| [g, @@genres.count(g)]}]
-  end
-
   def self.artists
     @@artists.uniq
   end
-  def self.artist_count
-    Hash[@@artists.uniq.collect {|a| [a, @@artists.count(a)]}]
+
+  def self.genres
+    @@genres.uniq
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
 end
