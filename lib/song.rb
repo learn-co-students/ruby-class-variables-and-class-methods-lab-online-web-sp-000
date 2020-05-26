5ae7c60ed7152886dd72aa722cbcac01697cba6c
Heath Mast <hrmast@gmail.com>
2020-05-26 04:04:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,45 @@
+require "pry"
+
 class Song
-attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
-@@count = 0
-@@artists = []
-@@genres = []
+    @@count = 0
+    @@artists = []
+    @@genres = []
 
-def initialize(name, artist, genres)
-  @name = name
-  @artist = artist
-  @genre = genres
-  @@count += 1
-  @@artists << @artist
-  @@genres << @genre
-end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
 
-def self.count
-  @@count
-end
+  def self.count
+    @@count
+  end
 
-def self.artists
-  @@artist
-end
+  def self.artists
+    @@artists.uniq
+  end
 
-def self.genres
-  @@genres
-end
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = Hash.new(0)
+    @@genres.each do |genre| genre_count[genre] += 1
+    end
+    genre_count
+  end
+
+
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each do |artist| artist_count[artist]+= 1
+    end
+    artist_count
+  end
 end
