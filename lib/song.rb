2477a7c9b0a39649e55c1eec3e199a1ff592dc1e
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-10 19:58:47 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -20,17 +20,41 @@ class Song
     def self.genres
      @@genres.uniq
    end
+
+   require "pry"
     def self.genre_count
-     genre_count = Hash.new(0)
-     @@genres.each {|genre| genre_count[genre] += 1}
-     genre_count
+     genre_count = {}
+     @@genres.each do |genre|  
+      if !genre_count[genre]
+    genre_count[genre] = 1
+      else 
+        genre_count[genre]+=1
+     end
    end
-    def self.artist_count
-     artist_count = Hash.new(0)
-     @@artists.each {|artist| artist_count[artist] += 1}
-     artist_count
-   end
-  end 
+   genre_count
+end
+
+
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist_genre|  
+     if artist_count[artist_genre]
+        artist_count[artist_genre] += 1
+     else 
+        artist_count[artist_genre] = 1  
+    end
+  end
+  artist_count
+end
+end
+
+
+
+
+
+
+
+
 
 
 
