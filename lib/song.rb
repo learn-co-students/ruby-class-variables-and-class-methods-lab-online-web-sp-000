7998f186a6539a5691096d8d47d0e5c82eb59391
Morgan Matney <morgan.matney89@gmail.com>
2020-08-12 23:20:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count.include?(genre)
+      if genre_count[genre]
         genre_count[genre] += 1
       else
         genre_count[genre] = 1
@@ -41,7 +41,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count.include?(artist)
+      if artist_count[artist]
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
