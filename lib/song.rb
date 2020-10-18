afe57d68d3d0e3306d5101bf3327aba87b54c8c6
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-18 02:28:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,11 @@
+require 'pry'
 class Song
   
-  @@count = 0
-  @@genres = []
-  @@artists = []
-  @@genre_histogram = {}
-  @@artist_histogram = {}
-  
   attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
@@ -21,32 +20,68 @@ class Song
     @@count
   end
   
-  def self.genres
-    @@genres.uniq
-  end
-    
   def self.artists
     @@artists.uniq
   end
   
-=begin
-  def self.genre_count
-    self.genres.each do |genre|
-      @@genre_histogram[genre] = @@genres.count(genre)
-    end
-    @@genre_histogram
+  def self.genres
+    @@genres.uniq
   end
-=end
   
   def self.genre_count
-    Hash[self.genres.map{|genre| [genre, @@genres.count(genre)]}]
+    hash = {}
+    self.genres.each do |genre|
+      hash[genre] = @@genres.count(genre)
+    end
+    hash
   end
   
   def self.artist_count
+    hash = {}
     self.artists.each do |artist|
-      @@artist_histogram[artist] = @@artists.count(artist)
+      hash[artist] = @@artists.count(artist)
     end
-    @@artist_histogram
+    hash
   end
-  
 end
+
+#   @@count = 0
+#   @@genres = []
+#   @@artists = []
+#   @@genre_histogram = {}
+#   @@artist_histogram = {}
+  
+#   attr_accessor :name, :artist, :genre
+  
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@count += 1
+#     @@artists << artist
+#     @@genres << genre
+#   end
+  
+#   def self.count
+#     @@count
+#   end
+  
+#   def self.genres
+#     @@genres.uniq
+#   end
+    
+#   def self.artists
+#     @@artists.uniq
+#   end
+  
+#   def self.genre_count
+#     Hash[self.genres.map{|genre| [genre, @@genres.count(genre)]}]
+#   end
+  
+#   def self.artist_count
+#     self.artists.each do |artist|
+#       @@artist_histogram[artist] = @@artists.count(artist)
+#     end
+#     @@artist_histogram
+#   end
+# end
---
+++ test.rb
@@ -0,0 +1,9 @@
+class Song
+  
+  @@count = 0
+  
+  def initialize
+    @@count += 1
+  end
+  
+end
