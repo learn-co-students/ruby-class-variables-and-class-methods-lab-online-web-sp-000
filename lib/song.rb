e164cec3aaead4278785c071905c3ba41b23f246
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-20 03:01:36 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_hash = {}
+  @@artist_hash = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genres.each do |genre|
+      @@genre_hash[genre] = @@genres.count(genre)
+    end
+    @@genre_hash
+  end
+
+  def self.artist_count
+    artists.each do |artist|
+      @@artist_hash[artist] = @@artists.count(artist)
+    end
+    @@artist_hash
+  end
+
+
+
+end
