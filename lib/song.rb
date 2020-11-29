1dd9a8c9a2c33c29c0e01fefe682979d78424e83
Jess Pesale <jess_pesale@aol.com>
2020-11-29 17:08:33 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,62 @@
+require 'pry'
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists << @artist
+        @@count += 1
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        new_artists = []
+        @@artists.each do |artist|
+            new_artists << artist unless new_artists.include? (artist)
+        end
+        new_artists
+    end
+
+    def self.genres
+        new_genres = []
+        @@genres.each do |genre|
+            new_genres << genre unless new_genres.include? (genre)
+        end
+        new_genres
+    end
+
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre] 
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+    end
+
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            if artist_count[artist]
+                artist_count[artist] += 1
+            else
+                artist_count[artist] = 1
+            end
+        end
+        artist_count
+    end
+end
