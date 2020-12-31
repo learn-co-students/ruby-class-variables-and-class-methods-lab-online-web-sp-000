5885682ed3a68e470aa9459f2ba2d0639f9e0d9e
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-31 21:38:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,28 @@
+require 'pry'
+
 class Song
- @@count=
- def initialize
-   @@attr_accessor(name, artist, genre)
+ @@count= 0 
+    @@artists = [ ]
+
+
+def self.artists
+  @@artists
+end 
+
+def self.name 
+  @@name 
+end 
+
+def self.genres 
+  @@genres 
+end 
+
+  def initialize(name, artist, genre)
+    @name= name
+    @artist= artist 
+    @genre= genre  
+    @@count +=1
    
+     
+    end 
+end 
