02903039072a5ad74b531d6b40a01be9e5526237
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-01 00:50:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,48 @@
 require 'pry'
 
 class Song
- @@count= 0 
-    @@artists = [ ]
-
-
-def self.artists
-  @@artists
-end 
-
-def self.name 
-  @@name 
-end 
-
-def self.genres 
-  @@genres 
-end 
-
+ attr_accessor :name, :artist, :genre 
+ @@count = 0 
+  
   def initialize(name, artist, genre)
-    @name= name
-    @artist= artist 
-    @genre= genre  
-    @@count +=1
+    @@name= name
+    @@artist= artist 
+    @@genre= genre  
+    @@count+=1
+  end 
+
+  def self.name 
+    @@name 
+  end 
+
+  def self.genre
+    @@genre
+  end 
+  
+  def self.artist
+    @@artist
+  end 
+
+  def self.variables
+   @@count = 0 
    
+  end 
+  
+  def self.artists
+    @@artists = []
+  end 
+  
+  def self.genres 
+    @@genres = []
+  end 
+  
+  def artist_count 
+    @@artist_count = {}
+  end 
+  
+  def genre_count 
+    @@genre_count = {}
+  end 
+  
      
-    end 
-end 
+end  
