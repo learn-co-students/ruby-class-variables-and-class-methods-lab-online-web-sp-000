1dbe6b1f9fc7269b3cb373f7b5290b257ceb92bd
HELEN HARMAN <hlharman@bellsouth.net>
2021-01-02 23:31:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,40 @@
 require 'pry'
 
 class Song
- attr_accessor :name, :artist, :genre 
+ attr_accessor :name, :artist, :genre, :artists, :genres 
  @@count = 0 
  
- 
 def initialize(name, artist, genre)
+    @@genres= genres 
+    @@artists= artists 
     @name= name
     @artist= artist 
     @genre= genre  
-    @@count +=1 
-    
+    @@count +=1
   end 
+
 
   def self.artists
     @@artists.uniq = []
-  end
+    return @@artists.uniq 
+    end 
+
   
   def self.genres 
-    @@genres.uniq = []   
-  end 
-  
-  def self.artist_count 
+    @@genres.uniq = []
+    return @@genres.uniq 
+   end 
+
+def self.count 
+  return @@count 
+end 
+
+
+def self.artist_count 
     @@artist_count = {}
     @@artist.each do |artist|
       if !artist_count[artist]
-        artist_count[artist] = 1 
+        artist_count[artist] = 1
       else 
       end 
     end 
