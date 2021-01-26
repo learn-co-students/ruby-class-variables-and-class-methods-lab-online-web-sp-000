ef551c1a4fe0a927e69d32e32fe9653df5fb32a5
Chris Baum <christopher.baum@valpo.edu>
2021-01-26 00:10:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+end
