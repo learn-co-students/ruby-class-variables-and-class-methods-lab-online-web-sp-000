e797dd5937ca9bf1d06c587cf3af559cd0563e85
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-03 20:47:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,44 +1,51 @@
-class Song
-	attr_accessor :name, :artist, :genre
-
-	@@count = 0
-	@@genres = []
-	@@artists = []
-
-	def initialize(name, artist, genre)
-	  @name = name
-	  @artist = artist
-	  @genre = genre
-		@@count += 1
-		@@genres << genre
-		@@artists << artist
-	end
-
-	def self.count
-		@@count
-	end
-
-	def self.genres
-		return @@genres.uniq
-	end
-
-	def self.artists
-		return @@artists.uniq
-	end
-
-	def self.genre_count
-		count = {}
-		@@genres.each do |genre|
-			count[genre] = @@genres.count(genre)
-		end
-		count
-	end
-
-	def self.artist_count
-		count = {}
-		@@artists.each do |artist|
-			count[artist] = @@artists.count(artist)
-		end
-		count
-	end
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << self.artist
+    @@genres << self.genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre_name|
+      if genre_count[genre_name]
+        genre_count[genre_name] += 1
+      else
+        genre_count[genre_name] = 1
+      end
+    end
+    genre_count
+  end
+  
+  def self.artist_count
+        artist_count = {}
+    @@artists.each do |artist_name|
+      if artist_count[artist_name]
+        artist_count[artist_name] += 1
+      else
+        artist_count[artist_name] = 1
+      end
+    end
+    artist_count
+  end
 end
