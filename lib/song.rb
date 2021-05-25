bc0382b6be257b88ed5ae94883f9fb55c97e0d4b
Harrison Price <hprice518@gmail.com>
2021-05-25 22:17:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,24 +4,20 @@ class Song
   
   @@count = 0
   @@artists = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1 
-    if @@artists.length >= 1 
-      @artist << @@artists
-    end
   end
   
   def self.count
     @@count
   end
+
+
   
-  def self.artists
-    @@artists
-  end
 
 end
   
