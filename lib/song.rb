1bbc80eef3efd854263ee178d1d9f61125f522bb
Harrison Price <hprice518@gmail.com>
2021-05-25 17:39:59 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,22 +3,21 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
-  @@artist = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1 
-    @@artist += 1
   end
   
   def self.count
     @@count
   end
   
-  def self.artist
-    @@artist
+  def self.artists
+    @@artists
   end
 
 end
