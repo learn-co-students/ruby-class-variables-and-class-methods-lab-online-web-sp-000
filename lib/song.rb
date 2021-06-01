be22c29541831dfb52ba85ada6ea158f9dba421c
Harrison Price <hprice518@gmail.com>
2021-06-01 22:06:39 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -39,10 +39,14 @@ class Song
     genre_count
   end
 
-  def self.artist_count
+ def self.artist_count
     artist_count = {}
-    if artist_count.has_key?
-      puts @@genres.to_histogram
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
     end
     artist_count
   end
